10455e52447f51fd79f191685a71dd66d72fc185
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2023-02-27 09:31:07 +1000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@ import math
 def calculator():
     while True:
         # Get the user's input
-        user_input = input("Enter a mathematical expression (e.g. 2 + 3) or type exit to exit: ")
+        user_input = input("Enter a type of maths (math, log, cos, sin, tan) or type exit to exit: ")
         
         # Exit the calculator if the user types 'exit'
         # Say value of pi if the user types pi
@@ -10,13 +10,16 @@ def calculator():
             break
         if user_input == 'pi':
             print(math.pi)
-
-        # Use try-except block to handle errors
-        try:
-            # Evaluate the expression and print the result
-            result = eval(user_input)
-            print(result)
-        except:
-            # Print an error message if the input is invalid
-            print("Invalid input, please try again.")    
+        if user_input == 'math':
+            # Use try-except block to handle errors
+            try:
+                # Evaluate the expression and print the result
+                result = eval(user_input)
+                print(result)
+            except:
+                # Print an error message if the input is invalid
+                print("Invalid input, please try again.")
+        if user_input == 'log':
+            math.log(1)
+             
 calculator()
